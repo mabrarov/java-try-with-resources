b97dba6749529167df42521c0fd1c16872abb3b2
Marat Abrarov <abrarov@gmail.com>
2019-08-27 14:28:23 +0300

    Enhanced test logs in case of failure

--- src/test/java/org/mabrarov/javatrywithresources/emulation/TestResource.java
+++ src/test/java/org/mabrarov/javatrywithresources/emulation/TestResource.java
@@ -2,28 +2,9 @@ package org.mabrarov.javatrywithresources.emulation;
 
 public class TestResource implements AutoCloseable {
 
-  private final int id;
-
-  public TestResource() {
-    this(0);
-  }
-
-  public TestResource(int id) {
-    this.id = id;
-  }
-
-  public int getId() {
-    return id;
-  }
-
   @Override
   public void close() throws Exception {
     // Nothing to do here
-  }
-
-  @Override
-  public String toString() {
-    return "TestResource{" + "id=" + id + '}';
   }
 
 }
